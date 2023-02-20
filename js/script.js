c86e192c07ed2619bbf8c6c93f35016eb6c3f0bc
Nursaidbek Qodirov <qnursaidbek@gmail.com>
2023-02-20 05:57:39 -0800

    i have finished gallery section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+window.addEventListener("DOMContentLoaded", ()=>{
+    const firstBtn = document.getElementById("first_content");
+    const secondBtn = document.getElementById("second_content");
+    const thirdBtn = document.getElementById("third_content");
+    const galleryContent = document.querySelectorAll("gallery-content");
+    document.querySelector("active_content").classList.add("show");
+    firstBtn.addEventListener('click', ()=>{
+        document.querySelector("active_content").classList.remove("show");
+        document.querySelector("active_content").classList.add("hide");
+        galleryContent[0].classList.remove("hide")
+        galleryContent[0].classList.add("show")
+    })
+})
